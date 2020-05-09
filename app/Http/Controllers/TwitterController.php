d7e8aa4e7fd36b2bcb0678f8b9d6e2911e13966d
yotaro__ok <yotaro.ok@gmail.com>
2020-05-10 05:07:34 +0900

    upd

---
+++ app/Http/Controllers/TwitterController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Abraham\TwitterOAuth\TwitterOAuth;
+
+class TwitterController extends Controller
+{
+    //
+    public function index(Request $request) {
+        return view('twitter')->with(['buttonLabel' => '認証', 'actionUrl' => '/twitterresetter/authenticate']);
+    }
+
+    public function authenticate(Request $request)
+    {
+        $twitter = new TwitterOAuth(
+            config('twitter.api_key'),
+            config('twitter.secret_key')
+        );
+
+        $token = $twitter->oauth('oauth/request_token', array(
+            'oauth_callback' => config('twitter.call_back_url')
+        ));
+
+        session(array(
+            'oauth_token' => $token['oauth_token'],
+            'oauth_token_secret' => $token['oauth_token_secret'],
+        ));
+
+        $url = $twitter->url('oauth/authenticate', array(
+            'oauth_token' => $token['oauth_token']
+        ));
+
+        return redirect($url);
+    }
+
+    public function callback(Request $request)
+    {
+        $oauth_token = session('oauth_token');
+        $oauth_token_secret = session('oauth_token_secret');
+
+        if ($request->has('oauth_token') && $oauth_token !== $request->oauth_token) {
+            return view('twitter')->with(['buttonLabel' => 'もう１回', 'actionUrl' => '/twitterresetter']);
+        }
+
+        $twitter = new TwitterOAuth(
+            $oauth_token,
+            $oauth_token_secret
+        );
+        $token = $twitter->oauth('oauth/access_token', array(
+            'oauth_verifier' => $request->oauth_verifier,
+            'oauth_token' => $request->oauth_token,
+        ));
+
+        $twitter_user = new TwitterOAuth(
+            config('twitter.api_key'),
+            config('twitter.secret_key'),
+            $token['oauth_token'],
+            $token['oauth_token_secret']
+        );
+
+        $twitter_user_info = $twitter_user->get('account/verify_credentials');
+        return view('twitter')
+                ->with([
+                    'buttonLabel' => 'リセット',
+                    'actionUrl' => '/twitterresetter/reset',
+                    'oauth_token' => $token['oauth_token'],
+                    'oauth_token_secret' => $token['oauth_token_secret'],
+                ]);
+    }
+
+    public function reset(Request $request)
+    {
+        $twitter_user = new TwitterOAuth(
+            config('twitter.api_key'),
+            config('twitter.secret_key'),
+            $request['oauth_token'],
+            $request['oauth_token_secret']
+        );
+
+        do {
+            $tweets = $twitter_user->get('statuses/user_timeline');
+            foreach ($tweets as $tweet) {
+                $twitter_user->post('statuses/destroy', ['id' => $tweet->id]);
+            }
+        } while (count($tweets) > 0);
+
+        do {
+            $favorites = $twitter_user->get('favorites/list');
+            foreach ($favorites as $favorite) {
+                $twitter_user->post('favorites/destroy', ['id' => $favorite->id]);
+            }
+        } while (count($favorites) > 0);
+
+        return view('twitter')->with(['buttonLabel' => '認証解除', 'actionUrl' => 'https://twitter.com/settings/applications']);
+    }
+}
---
+++ resources/views/twitter.blade.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# website: http://ogp.me/ns/websaite#">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <!-- ogp -->
+        <meta property="og:title" content="ついったーりせったー | yotaro.work" />
+        <meta property="og:url" content="https://yotaro.work/twitterresetter" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="ついったーりせったー" />
+        <meta property="og:description" content="ツイート、リツイート、いいねを一括削除します" />
+        <meta property="og:image" content="https://yotaro.work/images/skull.png" />
+        <!-- twitter -->
+        <meta name="twitter:title" content="ついったーりせったー | yotaro.work">
+        <meta name="twitter:card" content="summary_large_image">
+        <meta name="twitter:site" content="@yotaro_ok">
+        <meta name="twitter:creator" content="@yotaro_ok">
+        <meta name="twitter:description" content="ツイート、リツイート、いいねを一括削除します">
+        <meta name="twitter:image" content="https://yotaro.work/images/skull.png">
+
+        <title>yotaro.work</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;/reset
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body style="background-color: #ffffff; color: #000000">
+        <div class="flex-center position-ref full-height">
+            <div class="content">
+                <div class="title m-b-md">ついったーりせったー</div>
+                <form method="post" action="{{ $actionUrl }}">
+                    @csrf
+                    <button type="submit" class="btn btn-primary">{{ $buttonLabel }}</button>
+                    @isset($oauth_token)
+                    <input type="hidden" name="oauth_token" value="{{ $oauth_token }}">
+                    <input type="hidden" name="oauth_token_secret" value="{{ $oauth_token_secret }}">
+                    @endisset
+                </form>
+            </div>
+        </div>
+    </body>
+</html>
