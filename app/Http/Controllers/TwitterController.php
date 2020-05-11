81a3ebb1d99be148b6147493a766d463c5440ce8
yotaro__ok <yotaro.ok@gmail.com>
2020-05-12 00:47:43 +0900

    upd

--- app/Http/Controllers/TwitterController.php
+++ app/Http/Controllers/TwitterController.php
@@ -43,41 +43,41 @@ class TwitterController extends Controller
     public function callback(Request $request)
     {
         try {
-        $oauth_token = session('oauth_token');
-        $oauth_token_secret = session('oauth_token_secret');
+            $oauth_token = session('oauth_token');
+            $oauth_token_secret = session('oauth_token_secret');
 
-        if ($request->has('oauth_token') && $oauth_token !== $request->oauth_token) {
+            if ($request->has('oauth_token') and $oauth_token !== $request->oauth_token) {
+                return view('twitter')
+                        ->with([
+                            'buttonLabel' => 'もう１回',
+                            'actionUrl' => '/twitterresetter/authenticate',
+                        ]);
+            }
+
+            $twitter = new TwitterOAuth(
+                $oauth_token,
+                $oauth_token_secret
+            );
+            $token = $twitter->oauth('oauth/access_token', array(
+                'oauth_verifier' => $request->oauth_verifier,
+                'oauth_token' => $request->oauth_token,
+            ));
+
+            $twitter_user = new TwitterOAuth(
+                config('twitter.api_key'),
+                config('twitter.secret_key'),
+                $token['oauth_token'],
+                $token['oauth_token_secret']
+            );
+
+            $twitter_user_info = $twitter_user->get('account/verify_credentials');
             return view('twitter')
                     ->with([
-                        'buttonLabel' => 'もう１回',
-                        'actionUrl' => '/twitterresetter/authenticate',
+                        'buttonLabel' => 'リセット',
+                        'actionUrl' => '/twitterresetter/reset',
+                        'oauth_token' => $token['oauth_token'],
+                        'oauth_token_secret' => $token['oauth_token_secret'],
                     ]);
-        }
-
-        $twitter = new TwitterOAuth(
-            $oauth_token,
-            $oauth_token_secret
-        );
-        $token = $twitter->oauth('oauth/access_token', array(
-            'oauth_verifier' => $request->oauth_verifier,
-            'oauth_token' => $request->oauth_token,
-        ));
-
-        $twitter_user = new TwitterOAuth(
-            config('twitter.api_key'),
-            config('twitter.secret_key'),
-            $token['oauth_token'],
-            $token['oauth_token_secret']
-        );
-
-        $twitter_user_info = $twitter_user->get('account/verify_credentials');
-        return view('twitter')
-                ->with([
-                    'buttonLabel' => 'リセット',
-                    'actionUrl' => '/twitterresetter/reset',
-                    'oauth_token' => $token['oauth_token'],
-                    'oauth_token_secret' => $token['oauth_token_secret'],
-                ]);
         } catch(Exception $e) {
             return view('twitter')
                     ->with([
