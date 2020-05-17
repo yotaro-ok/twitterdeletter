7da6697407f8616137a50327d86bab9cb8f7f1c0
yotaro-ok <yotaro.ok@gmail.com>
2020-05-18 03:34:38 +0900

    upd

--- resources/views/twitter.blade.php
+++ resources/views/twitter.blade.php
@@ -107,8 +107,8 @@
                     @endisset
                 </form>
 		</br>
-		<div><p style="color: #FF0000"><small>本サービスをご利用の際は、</br>「すべて自己責任に於いてのご利用」</br>とさせて頂きます。</small></p></div>
-		<div><p style="color: #FF0000"><small>When you use this web service, you do so at your own risk.</small></p></div>
+		<div><small>本サービスをご利用の際は、</br>「すべて自己責任に於いてのご利用」</br>とさせて頂きます。</small></div>
+		<div><small>When you use this web service, you do so at your own risk.</small></div>
             </div>
         </div>
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
